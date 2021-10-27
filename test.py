564fdfb4cec9d0dc7ade997ff440527c1361a868
mchan02 <90062401+mchan02@users.noreply.github.com>
2021-10-27 13:17:10 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ print("Hello world!")
 print("This is not a math class, it is a thinking class")
 # Leilani's changes to the file :D
 print("Today is Wednesday hehehhe!")
+# mandy's changes to the file
+print("gaslight gatekeep girlboss B-)")
