636ba2b2641ee5dfa7d45cd296d9829ae194eb7b
Hanna Zakharenko <hanna@zakharenko.com>
2021-11-15 13:15:34 -0500

    project check in final

--- project_checkin.py
+++ project_checkin.py
@@ -1,3 +1,5 @@
+import random 
+
 class Human:
     """
     Basic Human class, framework for Zookeeper and Human class
@@ -110,6 +112,56 @@ class User(Human):
             this visit? ")
         with open(filepath, 'w', encoding = 'utf-8') as f:
             f.write(goals)
+
+class Zookeeper(Human): 
+    """
+    The Zookeeper class is a subclass of the Human class and inherits 
+        the __init__() method attributes of the Human class. It interacts  
+        with the User class and Animal class. 
+        The Zookeeper class includes a quiz method where the user can type
+        answers to quesetions asked by the Zookeeper. 
+        A seperate method will be made to calculate the user's quiz score. 
+    """
+    def quiz(self, user, file): 
+        """
+        Reads a file of quiz questions and answers. Prints a number 
+            (determined by the age of the user) of questions about animals in 
+            the Zoo for the user to type answers to. Records the questions  
+            asked and the user's answers to for later use. 
+        Args: 
+            user (User class object): the user. 
+            file (str): path to a file with quiz questions and answers. 
+        Returns: 
+            answer_list (list): a list of the user's responses the the questions
+        """
+        questions = {}
+        cor_answers = {}
+        asked_questions = []
+        answer_list = []
+
+        with open(file, "r", encoding = "UTF-8") as f:   
+            for line in f:  
+                line.strip()
+                line = line.split(":") 
+                n,q,a = line[0], line[1].strip(), line[2].strip()
+                questions[n] = q
+                cor_answers[n] = a
+        unasked_questions = set(questions.values()) - set(asked_questions)
+        if user.age > 18: 
+            for i in range(5): 
+                quest = random.choice(list(unasked_questions))
+                asked_questions.append(quest)
+                unasked_questions = set(questions.values()) - set(asked_questions)
+                answer = input(f"{quest}: ")
+                answer_list.append(answer)
+        else: 
+            for i in range(3): 
+                quest = random.choice(list(unasked_questions))
+                asked_questions.append(quest)
+                unasked_questions = set(questions.values()) - set(asked_questions)
+                answer = input(f"{quest}: ")
+                answer_list.append(answer)
+        return answer_list
 
 class Animal:
     """This class reads the file of animals in the zoo and organizes
