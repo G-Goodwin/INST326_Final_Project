30486c9fc793cc3e4cb765034d243d3a6c73c2d8
Hanna Zakharenko <hanna@zakharenko.com>
2021-12-13 00:10:35 -0500

    update animals

--- project_checkin.py
+++ project_checkin.py
@@ -17,7 +17,7 @@ class Human:
         """
         self.name = input("What is your name? ")
         self.age = int(input("How old are you? "))
-        
+        self.animals = Animals
         if self.age is not type(int) or self.age <= 0:
             raise TypeError("Please enter a valid age.")
         
@@ -138,7 +138,8 @@ class Zookeeper(Human):
         cor_answers = {}
         asked_questions = []
         answer_list = []
-
+        zoo = self.animals.zoo
+        
         with open(file, "r", encoding = "UTF-8") as f:   
             for line in f:  
                 line.strip()
@@ -163,15 +164,13 @@ class Zookeeper(Human):
                 answer_list.append(answer)
         return answer_list
 
-class Animal:
-    """This class reads the file of animals in the zoo and organizes them 
-        into a zoo dictionary.
+class Animals:
+    """This class reads the file of animals in the zoo and organizes them into a zoo dictionary.
     Attributes:
         filepath(str): a path to a file.
     """
     def __init__(self, filepath):
-        """Opens a file, unpacks it by line and appends it to a list as a
-            dictionary.
+        """Opens a file, unpacks it by line and appends it to a list as a dictionary.
         Args:
             filepath(str): a path to a file.
         Side Effects:
@@ -182,28 +181,41 @@ class Animal:
             for line in f:
                 line.rstrip("\n")
                 name, type1, eat, sleep, talk, play, fact = line.split(",")
-                x = {"name": name, "type": type1.lstrip(), "eat": eat, \
-                    "sleep":sleep, "talk": talk, "play": play, \
+                x = {"name": name.strip(" "), "type": type1.strip(" "), "eat": eat.strip(" "), \
+                    "sleep":sleep.strip(" "), "talk": talk.strip(" "), "play": play.strip(" "), \
                         "fact": fact.rstrip("\n")}
                 self.zoo.append(x)
-        #print(self.zoo)
-        self.action(self.zoo)
-    def action(self, animal):
+            self.zoo = self.zoo[1:]
+        # print(self.zoo)
+        # self.action(self.zoo)
+    def action(self, fle):
         copy_zoo = self.zoo
+        selection_list = []
+        selection_list2 = []
+        f = open(fle, "w", encoding="utf-8")
         for x in copy_zoo:
-            name = x["name"]
-            type1 = x["type"]
-            eat = x["eat"]
-            sleep = x["sleep"]
-            talk = x["talk"]
-            play = x["play"]
-            print("The name of the animal is " + name)
-            print("This animal is a " + type1)
-            print("This animal eats " + eat)
-            print("This animal sleeps " + sleep + " a day")
-            print("This animal says " + talk)
-            print("This animal plays by " + play)
-            #Turn print statements into return statements (try to mak more 
-            # complicated if possible), maybe read into a new file to create a
-            #fact sheet
-            
+            for x in copy_zoo:
+                name = x["name"]
+                type1 = x["type"]
+                eat = x["eat"]
+                sleep = x["sleep"]
+                talk = x["talk"]
+                play = x["play"]
+                selection_list.append("The " + name + " says " + talk + " to ")
+                selection_list.append("The " + name + " is able to " + play + " with ")
+                selection_list.append("The " + name + " sleeps " + sleep + " and ")
+                selection_list.append("The " + name + " eats " + eat + " and ")
+                selection_list.append("The " + name + " is a " + type1+ " and ")
+            for x in copy_zoo:
+                name = x["name"]
+                type1 = x["type"]
+                eat = x["eat"]
+                sleep = x["sleep"]
+                talk = x["talk"]
+                play = x["play"]
+                selection_list2.append("the " + name + " says " + talk)
+                selection_list2.append("the " + name + " that plays " + play)
+                selection_list2.append("the " + name + " that sleeps " + sleep)
+                selection_list2.append("the " + name + " eats " + eat)
+                selection_list2.append("the " + name + " that is a " + type1)
+            f.writelines(random.choice(selection_list)+random.choice(selection_list2)+"."+"\n")
